f3df7ddb87d8d07649adfddc548ad5f50387d104
javaee <javaee@javaee-VirtualBox>
2020-06-11 08:52:28 +0200

    Dump de la base de datos.

--- clasicos.sql
+++ clasicos.sql
@@ -34,10 +34,11 @@ CREATE TABLE `clasicos` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `modelo` varchar(50) DEFAULT NULL,
   `marca` varchar(25) NOT NULL,
-  `anio` int(4) NOT NULL,
+  `anio` int(11) NOT NULL,
   `foto` varchar(150) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `clasicos_UN` (`modelo`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +47,7 @@ CREATE TABLE `clasicos` (
 
 LOCK TABLES `clasicos` WRITE;
 /*!40000 ALTER TABLE `clasicos` DISABLE KEYS */;
-INSERT INTO `clasicos` VALUES (1,'Mustang Fastback','Ford',1967,'https://motokiller.files.wordpress.com/2013/04/mump_1101_10_ford_1967_mustang_gt_fastback_left_quarter.jpg'),(2,'Bel Air','Chevrolet',1957,'http://i.ebayimg.com/00/s/MTIwOFgxNjAw/z/He0AAOSwX61ZFKN0/$_57.JPG?set_id=8800005007'),(3,'Cobra','Shelby',1965,'https://cdn.barrett-jackson.com/staging/carlist/items/Fullsize/Cars/60975/60975_Front_3-4_Web.jpg'),(4,'Mercury','Ford',1951,'https://i.pinimg.com/originals/33/01/e4/3301e443ebdf1c2e0683b87333576183.jpg'),(5,'Fairlane 500 Skyliner','Ford',1957,'https://www.supercars.net/blog/wp-content/uploads/2016/04/1957_Ford_Fairlane500Skyliner1.jpg'),(6,'Gran Torino','Ford',1975,'http://carphotos.cardomain.com/images/0000/21/37/401273.JPG?v=16045488?v=3'),(7,'Charger 500','Dodge',1970,'https://i.ytimg.com/vi/VuhotvebUvU/maxresdefault.jpg');
+INSERT INTO `clasicos` VALUES (1,'Mustang Fastback','Ford',1967,'https://motokiller.files.wordpress.com/2013/04/mump_1101_10_ford_1967_mustang_gt_fastback_left_quarter.jpg'),(2,'Bel Air','Chevrolet',1957,'http://i.ebayimg.com/00/s/MTIwOFgxNjAw/z/He0AAOSwX61ZFKN0/$_57.JPG?set_id=8800005007'),(3,'Cobra','Shelby',1965,'https://cdn.barrett-jackson.com/staging/carlist/items/Fullsize/Cars/60975/60975_Front_3-4_Web.jpg'),(4,'Mercury','Ford',1951,'https://i.pinimg.com/originals/33/01/e4/3301e443ebdf1c2e0683b87333576183.jpg'),(5,'Fairlane 500 Skyliner','Ford',1957,'https://www.supercars.net/blog/wp-content/uploads/2016/04/1957_Ford_Fairlane500Skyliner1.jpg'),(6,'Gran Torino','Ford',1975,'http://carphotos.cardomain.com/images/0000/21/37/401273.JPG?v=16045488?v=3'),(7,'Charger 500','Dodge',1970,'https://i.ytimg.com/vi/VuhotvebUvU/maxresdefault.jpg'),(10,'DMC-12','DeLorean Motor Company',1981,'https://s1.cdn.autoevolution.com/images/gallery/DeLorean-DMC-12-3031_26.jpg');
 /*!40000 ALTER TABLE `clasicos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-08 13:45:29
+-- Dump completed on 2020-06-11  8:32:52
