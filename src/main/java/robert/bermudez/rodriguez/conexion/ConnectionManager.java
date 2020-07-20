39cd4e63ed1cb8f3dd71236eb7f8d73014f9c162
javaee <javaee@javaee-VirtualBox>
2020-07-20 09:46:28 +0200

    Configuración a través de un DataSource.

--- src/main/java/robert/bermudez/rodriguez/conexion/ConnectionManager.java
+++ src/main/java/robert/bermudez/rodriguez/conexion/ConnectionManager.java
@@ -6,27 +6,44 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class ConnectionManager {
 	
 	// Si se copia esta clase en otro proyecto, es necesario modificar la URL para indicarle la BBDD que se use en dicho proyecto.
 	// USUARIO y PASSWORD son las credenciales que se usan en Ubuntu para acceder a la BBDD. Se pueden encontrar en el archivo de
 	// texto leeme.txt, situado en el escritorio de Ubuntu.
-	private final static String URL = "jdbc:mysql://localhost/automoviles_clasicos";
-	private final static String USUARIO = "debian-sys-maint";
-	private final static String PASSWORD = "o8lAkaNtX91xMUcV";
+//	private final static String URL = "jdbc:mysql://localhost/automoviles_clasicos";
+//	private final static String USUARIO = "debian-sys-maint";
+//	private final static String PASSWORD = "o8lAkaNtX91xMUcV";
+	
+	/**
+	 * Configuración a través de un DataSource
+	 * @see src/main/webapp/META-INF/context.xml
+	 */
 
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException, NamingException {
 
 		Connection con = null;
 
-		// Comprueba que exista el .jar de MySQL.
+		// Comprobar que exista el .jar de MySQL.
 		Class.forName("com.mysql.jdbc.Driver");
 
-		// Establece conexión.
-		con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+		// Establecer la conexión del DriverManager. 
+		// con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+		
+		InitialContext initCtx = new InitialContext();
+		Context envCtx = (Context) initCtx.lookup("java:comp/env");
+		DataSource dataSource = (DataSource)envCtx.lookup("jdbc/super");
+		
+		// Establecer la conexión del pool.
+		con = dataSource.getConnection();
 		
 		return con;
-
 	};
 
 } // class
