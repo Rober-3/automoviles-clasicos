802a513a63ec76f33223540d017bcbe6546054ae
javaee <javaee@javaee-VirtualBox>
2020-07-20 08:14:31 +0200

    Cierre de sesión.

--- src/main/java/robert/bermudez/rodriguez/controller/LogoutController.java
+++ src/main/java/robert/bermudez/rodriguez/controller/LogoutController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutController
@@ -20,7 +21,14 @@ public class LogoutController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		session = null;
+		
+		request.setAttribute("alerta", new Alerta("success","Has cerrado sesión correctamente."));
+		
+		request.getRequestDispatcher("inicio").forward(request, response);
 		
 	} // doGet
 
