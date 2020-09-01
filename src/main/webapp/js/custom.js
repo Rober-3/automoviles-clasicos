40545d37ad30fd9db6e1f0bfbcb2a527d0ae9fca
javaee <javaee@javaee-VirtualBox>
2020-09-01 12:08:21 +0200

    Añadidos comentarios.

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -7,6 +7,43 @@ $(document).ready(function() {
     $('.tabla').DataTable();
 });
 
+function init() {
+	console.log('Documento cargado y listo.');
+	
+}
+
+
+// Script para encriptar la contraseña de usuario.
+
+/*
+ * function cifrar() {
+		
+		console.log('cifrar y conseguir hash');
+		
+		//recupero el valor de la contraseña del input a traves de su ID
+		var contrasenia = document.getElementById('pass').value;		
+		
+		//consigo el hash mediante la libreria incluida en el pie.jsp
+		var hash = md5(contrasenia);
+		
+		//guardo en el atributo value del input el codigo hash
+		document.getElementById('pass').value = hash;		
+		
+		// comprobar si hay que confirmar la contraseña
+		var inputRePass = document.getElementById('repass');
+		
+		// comprobar que exista el input#repass, si no existe tiene valor undefined
+		if ( inputRePass ){                        
+			
+			var rehash = md5(inputRePass.value);
+			inputRePass.value = rehash;
+		}	
+				
+		//enviar formulario
+		return true; // si ponemos false no se envia el formulario
+		
+}
+ */
 
 
 // Script para confirmar la eliminación de un registro.
