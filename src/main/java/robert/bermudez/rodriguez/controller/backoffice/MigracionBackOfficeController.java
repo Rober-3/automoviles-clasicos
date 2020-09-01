45f024623716d2115ca74702402cf92fdf2fc257
javaee <javaee@javaee-VirtualBox>
2020-09-01 12:12:37 +0200

    Controlador para el proceso de migración.

---
+++ src/main/java/robert/bermudez/rodriguez/controller/backoffice/MigracionBackOfficeController.java
@@ -0,0 +1,146 @@
+package robert.bermudez.rodriguez.controller.backoffice;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import robert.bermudez.rodriguez.conexion.ConnectionManager;
+
+/**
+ * Controlador para realizar un proceso de migración de un fichero de texto a la base de datos.
+ * Lee un .txt con 100 registros y los inserta en la base de datos..
+ */
+@WebServlet("/views/backoffice/migracion")
+public class MigracionBackOfficeController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(MigracionBackOfficeController.class);
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		LOG.trace("Inicio.");
+
+		final String SQL = "INSERT INTO usuarios (nombre,contrasena,imagen,id_rol) VALUES (?,'111','aaa',1);";
+		
+		// El problema de establecer esta ruta para el nombre del fichero es que si el proyecto se cuelga en un servidor o
+		// se lleva a otro ordenador no se encontrará: /home/javaee/eclipse-workspace es una ruta dentro de una carpeta de
+		// mi ordenador. Para resolverlo hay que incluir el fichero en src/main/resources.
+		
+		// String fichero = "/home/javaee/eclipse-workspace/automoviles-clasicos/personas.txt";
+		final String PATH_FICHERO = "personas.txt"; // Busca el fichero en la carpeta src/main/resources.
+		
+		String mensaje = "";
+		int numLeidas = 0;
+		int numInsertadas = 0;
+		int numErroneas = 0;
+		long tiempoInicio = System.currentTimeMillis();
+		long tiempoFin = 0;
+		
+		ArrayList<String>lineasErroneas = new ArrayList<String>();
+		
+		// Lógica de programación.
+
+		try (	
+				Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL);
+				
+				// Para encontrar el fichero en la carpeta src/main/resources hay que sustituir las dos líneas de abajo.
+				// FileReader fr = new FileReader(PATH_FICHERO);
+				// BufferedReader br = new BufferedReader(fr);
+				BufferedReader br = new BufferedReader(new FileReader(getClass().getClassLoader().getResource(PATH_FICHERO).getFile()));
+
+				){
+
+			// Al establecer una conexión en Java por defecto es autocomitable. Con esta línea se evita. Para guardar los
+			// cambios temporales es necesario hacer un commit.
+			con.setAutoCommit(false);
+
+			String linea = br.readLine(); // Lee la primera línea del fichero. Al no estar dentro del while se obvia.
+			LOG.trace("Recorrer linea a linea el fichero después de saltar la primera.");
+
+			while (null != (linea = br.readLine())) {
+
+				try { // En un principio no inclui este try-catch, pero sólo se leían 85 de las 100 líneas del fichero.
+
+					numLeidas++;
+
+					String[] campos = linea.split(";"); // Separar la linea leída con ";".
+
+					if (campos.length == 6) { // Si la línea tiene seis campos es correcta.
+
+						pst.setString(1, campos[0]);
+						LOG.debug(pst);
+
+						int affectedRows = pst.executeUpdate();
+
+						if (affectedRows == 1) {
+							numInsertadas++;
+							LOG.trace("Registro insertado.");
+
+						} else {
+							LOG.trace("No se ha podido insertar el registro.");
+						}
+
+					} else {
+						lineasErroneas.add(linea);
+						numErroneas ++;
+
+					} // if-else
+
+				} catch (Exception e) {
+					LOG.error(e);
+					mensaje = e.getMessage();
+				}
+
+			} // while
+
+			con.commit();
+			LOG.trace("Al finalizar, realizar un commit para guardar en la BBDD.");
+
+		} catch(Exception e) {
+			LOG.error(e);
+			mensaje = e.getMessage();
+			e.printStackTrace();
+
+		} finally {
+			request.setAttribute("fichero", PATH_FICHERO);
+			request.setAttribute("leidas", numLeidas);
+			request.setAttribute("insertadas",numInsertadas);
+			request.setAttribute("erroneas", numErroneas);
+			request.setAttribute("lineasErroneas", lineasErroneas);
+			request.setAttribute("mensaje", mensaje);
+			
+			tiempoFin = System.currentTimeMillis();
+			request.setAttribute("tiempo", tiempoFin - tiempoInicio);
+
+			request.getRequestDispatcher("resumen-migracion.jsp").forward(request, response);
+
+		} // try-catch-finally
+
+		LOG.trace("Fin");
+
+	} // doGet
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+} // class 
