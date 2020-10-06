db618d222e40035b4bac8086eed0449d31899f86
Rober-3 <rober_bero@hotmail.com>
2020-10-06 09:30:39 +0200

    Documentación.

---
+++ src/main/java/robert/bermudez/rodriguez/controller/backoffice/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Engloba todos los controladores para el funcionamiento del backoffice.
+ * 
+ * @author Roberto Bermúdez Rodríguez
+ * @version 1.0
+ */
+package robert.bermudez.rodriguez.controller.backoffice;
