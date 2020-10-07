4e8320bf81d8f878c71f7abb25eddc1086b0db0f
Rober-3 <rober_bero@hotmail.com>
2020-10-08 01:58:29 +0200

    Copias originales de index y de su controlador.

---
+++ src/main/java/robert/bermudez/rodriguez/controller/InicioControllerCopia.java
@@ -0,0 +1,84 @@
+package robert.bermudez.rodriguez.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import robert.bermudez.rodriguez.modelo.daoimpl.ClasicoDAOImpl;
+import robert.bermudez.rodriguez.modelo.daoimpl.MarcaDAOImpl;
+import robert.bermudez.rodriguez.modelo.pojo.Clasico;
+import robert.bermudez.rodriguez.modelo.pojo.Marca;
+
+@WebServlet("/inicio-copia")
+public class InicioControllerCopia extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(InicioControllerCopia.class);
+	private static final ClasicoDAOImpl clasicoDao = ClasicoDAOImpl.getInstance();
+	
+	// El DAO de las marcas sólo es necesario para llamar al método getAllWithClassics(). Para obtenerlas pinchando en el
+	// dropdown de la cabecera, es suficiente con el listener InicioAppListener.
+	private static final MarcaDAOImpl marcaDao = MarcaDAOImpl.getInstance();
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String paramId = request.getParameter("idMarca");
+		String marca = request.getParameter("marca");
+		
+		ArrayList<Marca> marcasConClasicos = new ArrayList<Marca>(); // HashMap de marcas.
+
+		try {
+			
+			// marcas = marcaDao.getAll(); Para llamar a este método no sería necesario el DAO de las marcas.
+			
+			if (paramId == null) { // Si no se reciben parámetros, se muestran todas las marcas ordenadas alfabéticamente.
+				
+				marcasConClasicos = marcaDao.getAllWithClassics();
+				
+			} else { // Si se reciben parámetros, muestra la marca correspondiente al idMarca.
+				
+				int idMarca = Integer.parseInt(paramId);
+				ArrayList<Clasico> clasicos = clasicoDao.getAllByMarca(idMarca, 10);
+				
+				request.setAttribute("clasicos", clasicos);
+				request.setAttribute("marca", marca); // La envía para mostrarla en pantalla.
+				
+			} // if-else
+
+		} catch (Exception e) {
+			LOG.error(e);
+
+		} finally {
+			request.setAttribute("marcasConClasicos", marcasConClasicos);
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+
+		} // try-catch-finally
+
+	} // doGet
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+
+	} // doPost
+
+	// Para obtener los clásicos en la vista da lo mismo emplear doGet que doPost, con puentear uno de los dos es suficiente.
+	// También se puede crear el método doProcess y llamarlo tanto desde el doGet como del doPost.
+
+	// private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	// request.setAttribute("clasicos", clasicoDao.getAll() );		
+	// request.getRequestDispatcher("index.jsp").forward(request, response);
+
+	// } // doProcess
+
+} // class
---
+++ src/main/webapp/index2.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<jsp:include page="includes/header.jsp">
+	<jsp:param name="pagina" value="Pagina principal" />
+	<jsp:param name="title" value="Pagina principal" />
+</jsp:include>
+
+<main>
+	<div class="container mt-5">
+
+		<c:if test="${empty marca}">
+			<h2 class="text-center">Marcas A-Z</h2>
+			<div>
+				<c:forEach items="${marcasConClasicos}" var="m">
+					<h4 class="mt-5">${m.marca}</h4>
+					<div class="row mt-5">
+						<c:forEach items="${m.clasicos}" var="c">
+							<div class="card bg-light mb-5 mx-auto" style="max-width: 18rem;">
+								<img src="${c.foto}" class="card-img-top" alt="Foto del clásico">
+								<div class="card-body">
+									<h5 class="card-title font-weight-bold text-center">${c.modelo}</h5>
+									<!-- <p class="card-text">Marca: <b>${c.marca.marca}</b></p> -->
+									<p class="card-text">Año: <b>${c.anio}</b></p>
+									<p class="card-text">Subido por: <b>${c.usuario.nombre}</b></p>
+									<a href="${c.foto}" target="_blank" class="card-link foto">Ver el clásico</a>
+								</div>
+							</div>
+						</c:forEach>
+					</div>
+					<hr>
+				</c:forEach>
+			</div>
+		</c:if>
+
+		<c:if test="${not empty marca}">
+			<h2 class="text-center">Clásicos de ${marca}</h2>
+			<div class="row mt-5">
+				<c:forEach items="${clasicos}" var="c">
+					<div class="card bg-light mb-5 mx-auto" style="max-width: 18rem;">
+						<img src="${c.foto}" class="card-img-top" alt="Foto del clásico">
+						<div class="card-body">
+							<h5 class="card-title font-weight-bold">${c.modelo}</h5>
+							<!-- <p class="card-text">Marca: <b>${c.marca.marca}</b></p> -->
+							<p class="card-text">Año: <b>${c.anio}</b></p>
+							<a href="${c.foto}" target="_blank" class="card-link foto">Ver el clásico</a></b>
+							<p class="card-text">Subido por: <b>${c.usuario.nombre}</b></p>
+						</div>
+					</div>
+				</c:forEach>
+			</div>
+		</c:if>
+
+	</div>
+</main>
+
+<%@include file="includes/footer.jsp"%>
