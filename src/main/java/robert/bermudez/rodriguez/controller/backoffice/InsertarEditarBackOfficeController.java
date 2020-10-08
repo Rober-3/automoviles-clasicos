0ef5de4fdbb9df0f794ad69fd9ce3a8221089796
Rober-3 <rober_bero@hotmail.com>
2020-10-08 02:04:09 +0200

    Controlador para insertar, editar y validar clásicos.

---
+++ src/main/java/robert/bermudez/rodriguez/controller/backoffice/InsertarEditarBackOfficeController.java
@@ -0,0 +1,58 @@
+package robert.bermudez.rodriguez.controller.backoffice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import robert.bermudez.rodriguez.controller.Alerta;
+import robert.bermudez.rodriguez.controller.frontoffice.InsertarFrontOfficeController;
+import robert.bermudez.rodriguez.modelo.daoimpl.ClasicoDAOImpl;
+
+/**
+ * Servlet implementation class InsertarEditarBackOfficeController
+ */
+@WebServlet("/views/fackoffice/insertar-editar")
+public class InsertarEditarBackOfficeController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(InsertarFrontOfficeController.class);
+	private static final ClasicoDAOImpl dao = ClasicoDAOImpl.getInstance();
+	
+       
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	Alerta alerta = new Alerta();
+    	
+    	String id = request.getParameter("id"); // Recogida de parámetros de backoffice/clasicos.jsp.
+    	
+    	try {
+    		
+    		int idModelo = Integer.parseInt(id);
+    		
+    		if (idModelo != 0) {
+				dao.validar(idModelo);
+				alerta = new Alerta("success", "Clásico validado con éxito.");
+			}
+    		
+		} catch (Exception e) {
+			LOG.error(e);
+
+		} finally {
+			request.setAttribute("alerta", alerta);
+			request.getRequestDispatcher("index.jsp").forward(request, response);	
+		}
+    	
+	} // doGet
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+} // class
