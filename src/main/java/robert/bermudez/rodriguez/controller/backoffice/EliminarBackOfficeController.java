5bca382807dfb39ed43cceb41d8600b113430089
Rober-3 <rober_bero@hotmail.com>
2020-10-08 09:43:40 +0200

    Controlador para eliminar clásicos en el backoffice.

---
+++ src/main/java/robert/bermudez/rodriguez/controller/backoffice/EliminarBackOfficeController.java
@@ -0,0 +1,49 @@
+package robert.bermudez.rodriguez.controller.backoffice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import robert.bermudez.rodriguez.controller.Alerta;
+import robert.bermudez.rodriguez.modelo.daoimpl.ClasicoDAOImpl;
+import robert.bermudez.rodriguez.modelo.pojo.Clasico;
+
+@WebServlet("/views/backoffice/eliminar")
+public class EliminarBackOfficeController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(EliminarBackOfficeController.class);
+	private static final ClasicoDAOImpl dao = ClasicoDAOImpl.getInstance();
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Clasico clasico = new Clasico();
+		Alerta alerta = new Alerta();
+
+		String id = request.getParameter("id"); // backoffice/clasicos.jsp
+
+		try {
+
+			int idModelo = Integer.parseInt(id);
+
+			clasico = dao.delete(idModelo);	
+			alerta = new Alerta("success", "Clásico eliminado con éxito:" + clasico.getModelo());
+
+		} catch (Exception e) {
+			LOG.error(e);
+
+		} finally {
+			request.setAttribute("alerta", alerta);
+			request.getRequestDispatcher("clasicos.jsp").forward(request, response);
+		}
+
+	} // doGet
+
+} // class
