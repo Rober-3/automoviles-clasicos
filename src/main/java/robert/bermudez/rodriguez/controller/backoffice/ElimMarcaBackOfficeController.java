d12330c2cff77bb7a5cd468fb942689c2472359c
Rober-3 <rober_bero@hotmail.com>
2020-10-26 22:42:23 +0100

    Controlador para eliminar marcas desde el BackOffice.

---
+++ src/main/java/robert/bermudez/rodriguez/controller/backoffice/ElimMarcaBackOfficeController.java
@@ -0,0 +1,48 @@
+package robert.bermudez.rodriguez.controller.backoffice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import robert.bermudez.rodriguez.controller.publico.Alerta;
+import robert.bermudez.rodriguez.modelo.daoimpl.MarcaDAOImpl;
+import robert.bermudez.rodriguez.modelo.pojo.Marca;
+
+@WebServlet("/views/backoffice/eliminar-marca")
+public class ElimMarcaBackOfficeController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(ElimMarcaBackOfficeController.class);
+	private static final MarcaDAOImpl dao = MarcaDAOImpl.getInstance();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Marca marca = new Marca();
+		Alerta alerta = new Alerta();
+
+		String id = request.getParameter("id"); // backoffice/marcas.jsp
+
+		try {
+
+			int idMarca = Integer.parseInt(id);
+
+			marca = dao.delete(idMarca);	
+			alerta = new Alerta("success", marca + " eliminada con éxito.");
+
+		} catch (Exception e) {
+			LOG.error(e);
+
+		} finally {
+			request.setAttribute("alerta", alerta);
+			request.getRequestDispatcher("marcas.jsp").forward(request, response);
+		}
+		
+	} // doGet
+
+} // class
