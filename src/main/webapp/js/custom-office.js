dd2f33e88f853383361fbee89f50862dd4ac2275
Rober-3 <rober_bero@hotmail.com>
2020-10-26 20:10:41 +0100

    Añadido script para desplegar la barra lateral en el front y en el back.

--- src/main/webapp/js/custom-office.js
+++ src/main/webapp/js/custom-office.js
@@ -8,8 +8,7 @@ $(document).ready(function() {
 });
 
 function init() {
-	console.log('Documento cargado y listo.');
-	
+	console.log('Documento cargado y listo.');	
 }
 
 
@@ -39,8 +38,7 @@ function init() {
 		}	
 				
 		//enviar formulario
-		return true; // si ponemos false no se envia el formulario
-		
+		return true; // si ponemos false no se envia el formulario		
 }
  */
 
@@ -59,5 +57,24 @@ function confirmar(parametro) {
 		console.debug('Previene o detiene el evento del ancla.');
 		event.preventDefault();
 	}
-	
 }
+
+
+//Script de Bootstrap para desplegar la barra lateral.
+(function($) {
+    "use strict";
+
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+            if (this.href === path) {
+                $(this).addClass("active");
+            }
+        });
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
