bd7911871c6f165fe661563ad1916688f35e203c
Rober-3 <rober_bero@hotmail.com>
2020-10-26 22:24:43 +0100

    Añadidos métodos.

--- src/main/java/robert/bermudez/rodriguez/modelo/daoimpl/MarcaDAOImpl.java
+++ src/main/java/robert/bermudez/rodriguez/modelo/daoimpl/MarcaDAOImpl.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 import robert.bermudez.rodriguez.conexion.ConnectionManager;
 import robert.bermudez.rodriguez.modelo.dao.MarcaDAO;
 import robert.bermudez.rodriguez.modelo.pojo.Clasico;
+import robert.bermudez.rodriguez.modelo.pojo.EstadisticasMarca;
 import robert.bermudez.rodriguez.modelo.pojo.Marca;
 import robert.bermudez.rodriguez.modelo.pojo.Usuario;
 
@@ -19,13 +20,13 @@ public class MarcaDAOImpl implements MarcaDAO {
 	
 	private static final Logger LOG = Logger.getLogger(MarcaDAOImpl.class);
 	
-	// Singleton
+	// SINGLETON
 	
-	private static MarcaDAOImpl INSTANCE = null;
-
 	private MarcaDAOImpl() {
 		super();
 	}
+	
+	private static MarcaDAOImpl INSTANCE = null;
 	
 	public static MarcaDAOImpl getInstance() {
 		
@@ -44,47 +45,52 @@ public class MarcaDAOImpl implements MarcaDAO {
 	// executeQuery devuelve un ResultSet.
 	private static final String SQL_GET_BY_ID =				"SELECT id, marca FROM marcas WHERE id = ?;";
 	private static final String SQL_GET_ALL =				"SELECT id, marca FROM marcas ORDER BY marca ASC;";
-	private static final String SQL_GET_ALL_VALIDADAS =		"SELECT id, marca FROM marcas WHERE fecha_aprobacion IS NOT NULL ORDER BY marca ASC;";
-	private static final String SQL_GET_ALL_SIN_VALIDAR =	"SELECT id, marca FROM marcas WHERE fecha_aprobacion IS NULL ORDER BY marca ASC;";
 	private static final String SQL_GET_ALL_WITH_CLASSICS = "SELECT m.id 'id_marca', marca, c.id 'id_modelo', modelo, anio, foto, u.id 'id_usuario', u.nombre 'nombre_usuario' " +
 															"FROM marcas m, clasicos c, usuarios u " + 
 															"WHERE m.id = id_marca " +
 															"AND u.id = m.id_usuario " +
 															"AND c.fecha_validacion IS NOT NULL " +
 															"ORDER BY marca ASC";
+	private static final String SQL_GET_BRAND_STATISTICS =	"SELECT total, aprobadas, pendientes FROM v_estadisticas_marcas;";
+
 	
 	// executeUpdate devuelve un int que representa el número de filas afectadas.
 	private static final String SQL_INSERT =				"INSERT INTO marcas (id, marca) VALUES (?,?);";
 	private static final String SQL_UPDATE =				"UPDATE marcas SET marca = ? WHERE id = ?;";
+	private static final String SQL_DELETE =		 		"DELETE FROM marcas WHERE id = ?;";
 	
 			
 	// MÉTODOS
 	
 	@Override
 	public Marca getById(int id) throws Exception {
-		
-		Marca marca = new Marca();
-		
-		try (	Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(SQL_GET_BY_ID);	) {
-			
-			pst.setInt(1, id);
-			ResultSet rs = pst.executeQuery();
-			
-			if (rs.next()) {
-				marca = mapper(rs);
-			}
-			
-		} catch (Exception e) {
-			throw new Exception("No existen en la base de datos marcas con el id " + id + ".");
 
-		} // try-catch
+		Marca marca = new Marca();
+
+		try (	Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_GET_BY_ID);
+				) {
+
+			pst.setInt(1, id);
+			LOG.debug(pst);
+
+			try (ResultSet rs = pst.executeQuery()) {
+
+				if (rs.next()) {
+					marca = mapper(rs);
+
+				} else {
+					throw new Exception("No existen en la base de datos marcas con el id " + id + ".");
+				}
+				
+			} // try interno
+			
+		} // try externo
 		
 		return marca;
-		
+
 	} // getById
 
-	
 	
 	@Override
 	public ArrayList<Marca> getAll() throws Exception {
@@ -95,10 +101,9 @@ public class MarcaDAOImpl implements MarcaDAO {
 				Connection con = ConnectionManager.getConnection();
 				PreparedStatement pst = con.prepareStatement(SQL_GET_ALL);
 				ResultSet rs = pst.executeQuery();
-				
 				){
 			
-			System.out.println(pst);
+			LOG.debug(pst);
 			
 			while (rs.next()) {
 				marcas.add(mapper(rs));
@@ -109,49 +114,6 @@ public class MarcaDAOImpl implements MarcaDAO {
 		return marcas;
 		
 	} // getAll
-	
-	
-	public ArrayList<Marca> getAllValidadas() throws Exception {
-		
-		ArrayList<Marca> marcas = new ArrayList<Marca>();
-
-		try (	Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(SQL_GET_ALL_VALIDADAS);
-				ResultSet rs = pst.executeQuery();
-				) {
-
-			LOG.debug(pst);
-
-			while (rs.next()) {
-				marcas.add(mapper(rs));	
-			}
-
-		} // try
-
-		return marcas;
-		
-	} // getAllValidadas
-
-	
-	public ArrayList<Marca> getAllSinValidar() throws Exception {
-		
-		ArrayList<Marca> marcas = new ArrayList<Marca>();
-
-		try (	Connection con = ConnectionManager.getConnection();
-				PreparedStatement pst = con.prepareStatement(SQL_GET_ALL_SIN_VALIDAR);
-				ResultSet rs = pst.executeQuery();
-				) {
-
-			LOG.debug(pst);
-
-			while (rs.next()) {
-				marcas.add(mapper(rs));	
-			}
-
-		} // try
-
-		return marcas;
-	} // getAllSinValidar
 
 	
 	@Override
@@ -213,6 +175,33 @@ public class MarcaDAOImpl implements MarcaDAO {
 		return new ArrayList<Marca>(marcas.values());
 		
 	} // getAllWithClassics
+	
+
+	@Override
+	public EstadisticasMarca getBrandStatistics() {
+
+		EstadisticasMarca estadisticasMarcas = new EstadisticasMarca();
+
+		try (	Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_GET_BRAND_STATISTICS);
+				ResultSet rs = pst.executeQuery()
+				) {
+
+			LOG.debug(pst);
+
+			while (rs.next()) { 
+				estadisticasMarcas.setTotal(rs.getInt("total"));
+				estadisticasMarcas.setAprobadas(rs.getInt("aprobadas"));
+				estadisticasMarcas.setPendientes(rs.getInt("pendientes"));
+			}
+
+		} catch (Exception e) {
+			LOG.error(e);	
+		}
+
+		return estadisticasMarcas;
+
+	} // getAllEstadisticasMarcas
 
 	
 	
@@ -251,23 +240,53 @@ public class MarcaDAOImpl implements MarcaDAO {
 	} // insert
 	
 	
-
 	@Override
 	public Marca update(Marca pojo) throws Exception {
-		//"UPDATE marcas SET marca = ? WHERE id = ?;";
+
+		try (	Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_UPDATE);
+				) {
+
+			pst.setString(1, pojo.getMarca());
+			pst.setInt(2, pojo.getId());
+			LOG.debug(pst);
+
+			int affectedRows = pst.executeUpdate();
+			if (affectedRows != 1) {
+				throw new Exception("No se han actualizado correctamente los datos de la marca " + pojo + ".");
+			}
+
+		} // try
+
 		return pojo;
 		
 	} // update
 	
 	
-
 	@Override
-	public Marca delete(int id) throws Exception {
-		// Las marcas no deben borrarse, porque aunque se eliminen todos sus
-		// clásicos deben estar siempre disponibles para nuevos modelos.
-		return null;
+	public Marca delete(int idMarca) throws Exception {
+		
+		Marca marca = getById(idMarca);
+		
+		try (	Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_DELETE);
+				) {
+
+			pst.setInt(1, idMarca);
+			LOG.debug(pst);
+
+			int affectedRows = pst.executeUpdate();
+			if (affectedRows != 1) {
+				throw new Exception("No existen en la base de datos clásicos con el id " + idMarca + ".");
+			}
+
+		} catch (Exception e) {
+			throw new Exception("Ha habido un problema al tratar de borrar " + marca + ".");
+		}
+		
+		return marca;
+		
 	} // delete
-	
 	
 	
 	public Marca mapper(ResultSet rs) throws SQLException {
