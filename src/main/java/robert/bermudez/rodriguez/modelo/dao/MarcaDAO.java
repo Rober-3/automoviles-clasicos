4ff1ef07e75de831112031c663acf8d78a1ff3af
Rober-3 <rober_bero@hotmail.com>
2020-10-26 22:25:17 +0100

    Añadidos nuevos métodos.

--- src/main/java/robert/bermudez/rodriguez/modelo/dao/MarcaDAO.java
+++ src/main/java/robert/bermudez/rodriguez/modelo/dao/MarcaDAO.java
@@ -3,18 +3,16 @@ package robert.bermudez.rodriguez.modelo.dao;
 import java.util.ArrayList;
 
 import robert.bermudez.rodriguez.interfaces.CrudAble;
+import robert.bermudez.rodriguez.modelo.pojo.EstadisticasMarca;
 import robert.bermudez.rodriguez.modelo.pojo.Marca;
 
 public interface MarcaDAO extends CrudAble<Marca>{
-	
-	public ArrayList<Marca> getAllValidadas() throws Exception;
-	
-	public ArrayList<Marca> getAllSinValidar() throws Exception;
 	
 	/**
 	 * Obtiene todas las marcas con sus clásicos asociados.
 	 * @return ArrayList de marcas ordenadas alfabéticamente.
 	 */
-	public ArrayList<Marca> getAllWithClassics(); // Método para hacer el hashmap de clásicos.
-
+	ArrayList<Marca> getAllWithClassics(); // Método para hacer el hashmap de clásicos.
+	
+	EstadisticasMarca getBrandStatistics();
 }
