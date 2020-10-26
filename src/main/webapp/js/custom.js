2418a343cc9ee75116c409020cbde99fc9ee6103
Rober-3 <rober_bero@hotmail.com>
2020-10-26 22:07:49 +0100

    Limpieza de código.

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -94,19 +94,15 @@ function showHidePass(idElement) {
 
 
 // Script para confirmar la eliminación de un registro.
-
 function confirmar(modelo) {
 	
 	// The confirm() method returns true if the user clicked "OK", and false otherwise.
 	// modelo es el parámetro que se le pasa al script desde la vista ('${a.modelo}).
 	if ( confirm('¿Estás seguro de querer eliminar ' + modelo + '?') ){
-		
 		console.debug('Continúa el evento por defecto del ancla.');
 		
 	}else {
-		
 		console.debug('Previene o detiene el evento del ancla.');
 		event.preventDefault();
 	}
 }
-
