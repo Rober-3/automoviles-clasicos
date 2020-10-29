ed62c7731fa1b39e599927e1d4184058b3b4c6d1
Rober-3 <rober_bero@hotmail.com>
2020-10-29 09:54:25 +0100

    Añadida seguridad al borrado de un clásico.

--- src/main/java/robert/bermudez/rodriguez/controller/backoffice/ElimClasBackOfficeController.java
+++ src/main/java/robert/bermudez/rodriguez/controller/backoffice/ElimClasBackOfficeController.java
@@ -11,36 +11,53 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import robert.bermudez.rodriguez.controller.publico.Alerta;
+import robert.bermudez.rodriguez.modelo.dao.SeguridadException;
 import robert.bermudez.rodriguez.modelo.daoimpl.ClasicoDAOImpl;
+import robert.bermudez.rodriguez.modelo.daoimpl.UsuarioDAOImpl;
 import robert.bermudez.rodriguez.modelo.pojo.Clasico;
+import robert.bermudez.rodriguez.modelo.pojo.Usuario;
 
 @WebServlet("/views/backoffice/eliminar-clasico")
 public class ElimClasBackOfficeController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = Logger.getLogger(ElimClasBackOfficeController.class);
-	private static final ClasicoDAOImpl dao = ClasicoDAOImpl.getInstance();
+	private static final ClasicoDAOImpl daoClasico = ClasicoDAOImpl.getInstance();
+	private static final UsuarioDAOImpl daoUsuario = UsuarioDAOImpl.getInstance();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		Clasico clasico = new Clasico();
-		Alerta alerta = new Alerta();
+		Usuario usuario = new Usuario();
+		Alerta alerta = null;
 
 		String id = request.getParameter("id"); // views/backoffice/clasicos.jsp
 
 		try {
 
 			int idModelo = Integer.parseInt(id);
-
-			clasico = dao.delete(idModelo);	
-			alerta = new Alerta("success", "Clásico eliminado con éxito:" + clasico.getModelo());
+			usuario = (Usuario)request.getSession().getAttribute("usuario");
+			
+			// Comprueba que sea el administrador quien efectúa el borrado.
+			if (daoUsuario.checkRole(usuario)) {
+				clasico = daoClasico.delete(idModelo);	
+				alerta = new Alerta("success",  clasico.getMarca().getMarca() + " " + clasico.getModelo() + " borrado con éxito.");
+				
+			} else {
+				throw new SeguridadException();
+			}
 
 		} catch (Exception e) {
 			LOG.error(e);
 
 		} finally {
-			request.setAttribute("alerta", alerta);
-			request.getRequestDispatcher("clasicos.jsp").forward(request, response);
+			request.getSession().setAttribute("alerta", alerta);
+			response.sendRedirect(request.getContextPath() + "/views/backoffice/clasicos");
+			
+			// Si se hace getRequestDispatcher no mostrará la lista con todos los clásicos. Hay
+			// que realizar una nueva petición para pasar por el controlador y recuperarlos.
+			// request.setAttribute("alerta", alerta);
+			// request.getRequestDispatcher("clasicos.jsp").forward(request, response);
 		}
 
 	} // doGet
