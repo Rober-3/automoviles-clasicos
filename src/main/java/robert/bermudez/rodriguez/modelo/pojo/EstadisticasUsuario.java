ef1251d0dcc30dd5bd4e1092bbe66ec4b76a72a9
Rober-3 <rober_bero@hotmail.com>
2020-11-02 10:33:52 +0100

    Pojo para mostrar el número de usuarios en el index del backoffice.

---
+++ src/main/java/robert/bermudez/rodriguez/modelo/pojo/EstadisticasUsuario.java
@@ -0,0 +1,24 @@
+package robert.bermudez.rodriguez.modelo.pojo;
+
+public class EstadisticasUsuario {
+	
+	private int total;
+
+	public EstadisticasUsuario() {
+		super();
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	@Override
+	public String toString() {
+		return "EstadisticasUsuario [total=" + total + "]";
+	}
+	
+}
